6b4eac733d211929c2f76aeb9160c981f5773ffe
tty13 <git@tty13.net>
2025-01-27 18:04:00 +0100

    divide credentials in multiple files for convenience

--- debian-12/build.sh
+++ debian-12/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-packer build -var-file='./credentials.pkr.hcl' ./debian-12.pkr.hcl
+packer build -var-file='../proxmox_api_credentials.pkr.hcl' -var-file='./credentials.pkr.hcl' ./debian-12.pkr.hcl
